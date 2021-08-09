3d9d657f7cc07e5a5047e26348dcdada1081dfd6
DivisionByzero <dustinjaehnig@outlook.de>
2021-08-09 18:22:08 +0200

    remove build script

--- scripts/build.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const childProcess = require('child_process');
-
-const htmlTemplateFileName = 'index.template.html';
-childProcess.execSync('npx webpack').toString();
-fs.copyFileSync(`src/${htmlTemplateFileName}`, `build/${htmlTemplateFileName}`)
