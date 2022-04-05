dc3bf60889c8333fdcf87723389283072f252a68
xDivisionByZerox <leyla.jaehnig@gmx.de>
2022-04-05 22:38:07 +0200

    refactor: scope spinner style

--- src/frontend/components/spinner.component.ts
+++ src/frontend/components/spinner.component.ts
@@ -1,6 +1,27 @@
 export function createSpinnerComponent(): HTMLDivElement {
   const spinner = document.createElement('div');
-  spinner.classList.add('spinner');
+
+  const size = '2rem';
+  spinner.style.width = size;
+  spinner.style.height = size;
+
+  spinner.style.borderRadius = '50%';
+  spinner.style.borderStyle = 'solid';
+  spinner.style.borderWidth = '0.25em';
+
+  spinner.style.borderBottomColor = 'currentColor';
+  spinner.style.borderLeftColor = 'currentColor';
+  spinner.style.borderRightColor = 'transparent';
+  spinner.style.borderTopColor = 'currentColor';
+
+  let rotation = 0;
+  const intervalTime = 800;
+  const intervalSteps = 20;
+  const step = 100 / intervalSteps;
+  setInterval(() => {
+    rotation = (rotation + step) % 360;
+    spinner.style.transform = `rotate(${rotation}deg)`;
+  }, intervalTime / intervalSteps);
 
   return spinner;
 }
